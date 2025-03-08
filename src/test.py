d9798a3082f145242a64144f2f91a72db897c809
dujunjie29 <57098646+dujunjie29@users.noreply.github.com>
2025-03-08 23:11:03 +0800

    test.py

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ import requests
 
 def main():
     render = Text2ImgRender()
-    template = """<html><head><meta name="viewport" content="width=device-width,initial-scale=1.0"></head><body><h1>{{ title }}😤</h1><p>{{ content }}</p></body></html>"""
+    template = """<html><head><meta name="viewport" content="width=device-width,initial-scale=1.0"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@latest/dist/katex.min.css"><script defer src="https://cdn.jsdelivr.net/npm/katex@latest/dist/katex.min.js"></script><script defer src="https://cdn.jsdelivr.net/npm/katex@latest/dist/contrib/auto-render.min.js"></script></head><body><h1>{{ title }}😤</h1><p>{{ content|safe }}</p></body></html>"""
     data = {"title": "Hello, World!", "content": "This is a test."}
     html = render.from_jinja_template(template, data)
     pic = render.html2pic(html, ScreenshotOptions(type="jpeg", full_page=True))
@@ -14,10 +14,12 @@ def main():
 def test_api():
     url = "http://localhost:8000/text2img/generate"
     data = {
-        "html": "<html><head><meta name=\"viewport\" content=\"width=device-width,initial-scale=1.0\"></head><body><h1>Hello, World!😤</h1><p>This is a test.</p></body></html>",
+        "html": "<html><head><meta name=\"viewport\" content=\"width=device-width,initial-scale=1.0\"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@latest/dist/katex.min.css"><script defer src="https://cdn.jsdelivr.net/npm/katex@latest/dist/katex.min.js"></script><script defer src="https://cdn.jsdelivr.net/npm/katex@latest/dist/contrib/auto-render.min.js"></script></head><body><h1>Hello, World!😤</h1><p>This is a test.</p></body></html>",
         "options": {
-            "type": "jpeg",
-            "full_page": True
+            "type": "png",
+            "full_page": True,
+            "timeout"=5000,
+            "omit_background"=True,
         },
         "json": False
     }
